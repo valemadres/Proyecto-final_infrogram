2f7b2474170b6728baaffec392254cd174243840
Noel Vallejos Ugarte <noel.72712510.va@gmail.com>
2022-10-12 20:12:55 -0400

    TextView Clickable

--- app/src/main/java/com/example/infogram/LoginActivity.java
+++ app/src/main/java/com/example/infogram/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.infogram;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+    public void goCreateAccount(View view) {
+        System.out.println("-------go to blablablaba");
+
     }
 }
