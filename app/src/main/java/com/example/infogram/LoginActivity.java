2788ae72d66d921915b001c01128a81835535d6f
Noel Vallejos Ugarte <noel.72712510.v@>
2022-11-09 12:30:25 -0400

    Intent

--- app/src/main/java/com/example/infogram/LoginActivity.java
+++ app/src/main/java/com/example/infogram/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.infogram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -13,7 +14,8 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
     }
     public void goCreateAccount(View view) {
-        System.out.println("-------go to blablablaba");
+        Intent intent = new Intent(this , CreateAccountActivity.class);
+        startActivity(intent);
 
     }
 }
