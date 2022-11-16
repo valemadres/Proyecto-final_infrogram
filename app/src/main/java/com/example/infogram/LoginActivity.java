058f37b775a16ce8a9c42241c1cf85d4b024e229
Noel Vallejos Ugarte <noel.72712510.v@>
2022-11-16 11:27:00 -0400

    Fragments

--- app/src/main/java/com/example/infogram/LoginActivity.java
+++ app/src/main/java/com/example/infogram/LoginActivity.java
@@ -3,8 +3,12 @@ package com.example.infogram;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.DrawableContainer;
 import android.os.Bundle;
 import android.view.View;
+
+import com.example.infogram.view.CotainerActivity2;
+import com.example.infogram.view.CreateAccountActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -17,5 +21,10 @@ public class LoginActivity extends AppCompatActivity {
         Intent intent = new Intent(this , CreateAccountActivity.class);
         startActivity(intent);
 
+    }
+
+    public void login(View view) {
+        Intent intent = new Intent(this, CotainerActivity2.class);
+        startActivity(intent);
     }
 }
